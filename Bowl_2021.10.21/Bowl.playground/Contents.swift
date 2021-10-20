9f0490f96393e26b2e7a0887cd9a49584fce7827
devhaute <devhaute@gmail.com>
2021-10-20 20:47:16 +0900

    그릇 문제 풀이

--- Bowl_2021.10.21/Bowl.playground/Contents.swift
+++ Bowl_2021.10.21/Bowl.playground/Contents.swift
@@ -1,1 +1,24 @@
+import Foundation
+
+func main(_ bowls: [String]) -> Int {
+    var weight = 10
+    
+    for (index, _) in bowls.enumerated() {
+        guard index > 0 else {
+            continue
+        }
+        
+        if bowls[index-1] == bowls[index] {
+            weight += 5
+        } else {
+            weight += 10
+        }
+    }
+    
+    
+    return weight
+}
+
+let bowls = ["(", ")", "(", ")", "(", ")", ")", ")", "("]
+print(main(bowls))
 
