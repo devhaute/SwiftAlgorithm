503d15e832d2ed9aa31459e93ae67f83a0125459
devhaute <devhaute@gmail.com>
2021-11-12 21:53:25 +0900

    마라톤 문제 구현

--- Marathon_2021.11.12/Marathon.playground/Contents.swift
+++ Marathon_2021.11.12/Marathon.playground/Contents.swift
@@ -1,3 +1,18 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+func main(_ participant: [String], _ completion: [String]) -> Void {
+    var dropouts = participant
+    
+    for aCompletion in completion {
+        if let removeIndex = dropouts.firstIndex(of: aCompletion) {
+            dropouts.remove(at: removeIndex)
+        }
+    }
+    
+    print("탈락자들: \(dropouts)")
+}
+
+let participant = ["mislav", "stanko", "mislav", "ana"]
+let completion = ["stanko", "ana", "mislav"]
+
+main(participant, completion)
